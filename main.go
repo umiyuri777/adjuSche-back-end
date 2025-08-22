35fef3f9daf08be974480c49a4bfc3039a4eb5d6
kusu0123 <is0825pr@ed.ritsumei.ac.jp>
2025-08-22 23:41:12 +0900

    supabaseとの接続

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 // Ginを使用した場合の例
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "adjuSche-back-end/repository"
+)
 
 func main() {
     r := gin.Default()
     r.GET("/hello", getHello)
 	r.GET("/getSchedule", getSchedule)
+    r.GET("/test", func(c *gin.Context) {
+        repo, err := repository.NewSupabaseRepository()
+        if err != nil {
+            c.String(500, "Error connecting to database: %v", err)
+            return
+        }
+        repo.CreateUserTable()
+        c.String(200, "User table created successfully")
+    })
     r.Run(":8080")
 }
 
